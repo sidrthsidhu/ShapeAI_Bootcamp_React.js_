a76c3bbf2ed74651b299d67bc66e15ca470cc78b
sidrthsidhu <86591933+sidrthsidhu@users.noreply.github.com>
2021-06-28 02:42:43 -0500

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p>THE BOOTCAMP WAS VERY INFORMATIVE AND UNDERSTOOD VERY WELL</p>
+    </div>
+  );
+}
+
+export default Info;
